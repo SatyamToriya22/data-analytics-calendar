cc0dd424c6ec1282561e3c51845ca44b08b3c5b7
satyam <toriyasatyam2@gmail.com>
2025-11-08 14:53:31 +0530

    initial commit - creating react app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='data-analytics-calendar'>
+        <h1
+          className='title'
+          style={{ fontSize: '36px', fontWeight: 'bold', marginBottom: '8px' }}
+        >
+          Data Analytics Calendar
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
